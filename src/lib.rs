ec12eb74d3587fc3e2a1736e29933f8bacbd5848
toshiaki <toshi.1026.1026@gmail.com>
2020-08-02 15:24:08 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// #[wasm_bindgen]
+// extern {
+//     fn alert(s: &str);
+// }
+
+// #[wasm_bindgen]
+// pub fn greet(name: &str) {
+//     // let sentence: String = "Hello ".to_string() + name + " wasm-game-of-life!";
+//     alert(&format!("Hello {}, wasm-game-of-life!", name));
+// }
+
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
